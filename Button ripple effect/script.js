2b879a9417fab6fcb005daedadeaacb63ace7d7e
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-11 17:11:51 +0500

    button ripple effect issues with circle not removed circle is fixed

--- Button ripple effect/script.js
+++ Button ripple effect/script.js
@@ -16,5 +16,8 @@ buttons.forEach((button) => {
 		circle.style.top = yInside + 'px';
 		circle.style.left = xInside + 'px';
 		this.appendChild(circle);
+		setTimeOut(() => {
+			circle.remove();
+		}, 500);
 	});
 });
